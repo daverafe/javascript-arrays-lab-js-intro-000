5cbce2ebbf534db52c781cf0ee4f6908f61882ae
David Rafe <david.rafe@gmail.com>
2018-10-21 00:28:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function prependKitten(name) {
   return kittens.slice(3, 'Arnold')
 }
 function removeLastKitten(name) {
-  kittens.slice(0)
-  return kittens.slice(0)
+  kittens.slice(0, 1)
+  return kittens.slice(0, 1)
 }
 function removeFirstKitten(name) {
   kittens.slice(1)
