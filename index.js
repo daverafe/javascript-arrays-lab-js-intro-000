05c839153bb858e88a1eeb1c849a060b31ebde70
David Rafe <david.rafe@gmail.com>
2018-10-21 00:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph')
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  kittens.concat('Broom')
+  return kittens.concat('Broom')
+}
+function prependKitten(name) {
+  kittens.splice(0, 0 'Arnold')
+  return kittens
+}
